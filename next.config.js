cf2c984a318710afbb63cf37d50f828f97f1a833
esot321c <86281023+esot321c@users.noreply.github.com>
2021-11-13 18:50:39 -0800

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    // CHANGE FOR PRODUCTION VERSION
-    // API_URL: 'https://ergopad.io/api',
-    API_URL: 'http://192.168.1.80:8000/api',
-  },
-}
